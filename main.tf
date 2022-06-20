b930e8c24c600028bdb17e4b296ad3fdb32913cf
vikobi <victorwonu@gmail.com>
2022-06-20 15:01:52 +0100

    added data sources to file

--- main.tf
+++ main.tf
@@ -20,3 +20,18 @@ resource "aws_subnet" "dev-subnet-1" {
     Name = "dev subnet test 1"
   }
 }
+
+data "aws_vpc" "existing" {
+  cidr_block        = "172.31.0.0/16"
+  }
+
+resource "aws_subnet" "dev-subnet-2" {
+  vpc_id            = data.aws_vpc.existing.id
+  availability_zone = "eu-west-2b"
+  cidr_block        = "172.31.0.0/20"
+
+  tags = {
+
+      Name = "addon default subnet"
+  }
+}
